717bf0bad6f44cf30b2826726ea67197fda19464
b45i <amalshajan2011@gmail.com>
2021-02-16 12:24:32 +0530

    experss API

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 const express = require('express');
 const cors = require('cors');
 const x = require('x-ray-scraper');
-const fs = require('fs');
 
 const app = express();
 app.use(cors());
 app.options('*', cors());
 const port = process.env.PORT || 5000;
 
-URL = 'https://github.com/trending';
+const BASE_URL = 'https://github.com/trending';
 
-x(URL, '.Box-row', [
-    {
-        repo: 'h1 a@href',
-        description: 'p',
-        language: 'div.f6 span:nth-child(2)',
-        stars: 'div.f6 a:nth-child(2)',
-        forks: 'div.f6 a:nth-child(3)',
-        starChanges: 'div.f6 span.float-sm-right',
-        contributors: x('div.f6 span.d-inline-block.mr-3', ['a@href']),
-    },
-]).then(res => {
-    const data = (res || []).map(r => {
+async function getTrends(url) {
+    res = await x(url, '.Box-row', [
+        {
+            repo: 'h1 a@href',
+            description: 'p',
+            language: 'div.f6 span:nth-child(2)',
+            stars: 'div.f6 a:nth-child(2)',
+            forks: 'div.f6 a:nth-child(3)',
+            starChanges: 'div.f6 span.float-sm-right',
+            contributors: x('div.f6 span.d-inline-block.mr-3', ['a@href']),
+        },
+    ]);
+
+    return (res || []).map(r => {
         return {
             ...r,
             description: (r.description || '').trim(),
@@ -30,9 +31,18 @@ x(URL, '.Box-row', [
             starChanges: (r.starChanges || '').trim(),
         };
     });
-    fs.writeFileSync('results.json', JSON.stringify(data));
+}
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 });
-// .write('results.json');
+
+app.get('/trending/:lang?', async (req, res) => {
+    const url = `${BASE_URL}/${req.params.lang ? req.params.lang : ''}${
+        req.query.since ? '?' + req.query.since : ''
+    }`;
+    res.send(await getTrends(url));
+});
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
